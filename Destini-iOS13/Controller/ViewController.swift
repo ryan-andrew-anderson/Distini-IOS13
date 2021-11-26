5a4cc81a311238a482c20b5ea09cf7439b5e62ff
Ryan A. Anderson <ryan.anderson9921@stu.mtec.edu>
2021-11-26 08:56:25 -0700

    Updating Story 4
    UI Changes
    Configuing Buttons

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,16 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    var timer = Timer()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
-
-
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        if sender.currentTitle == storyBrain.stories[storyBrain.storyNumber].choice1 {
+            storyLabel.text = "You go about your day and forget your enconter with the man"
+        } else {
+            storyLabel.text = "You offer the man your lunch and $20. The man expresses his thanks and you feel a little but better becuase you improved someones life today. "
+        }
+        sender.isSelected = true
+        timer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    }
+    
+    @objc func updateUI() {
+        choice1Button.isSelected = false
+        choice2Button.isSelected = false
+        
+        storyLabel.text = storyBrain.stories[storyBrain.storyNumber].title
+        choice1Button.setTitle(storyBrain.stories[storyBrain.storyNumber].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.stories[storyBrain.storyNumber].choice2, for: .normal)
+    }
 }
-
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+
+struct Story {
+    var title : String
+    var choice1 : String
+    var choice2 : String
+    var choice1Destination : Int
+    var choice2Destination : Int
+    
+    init(Title : String, Choice1: String, Choice2: String, Choice1Result: Int, Choice2Result: Int) {
+        
+        self.title = Title
+        self.choice1 = Choice1
+        self.choice2 = Choice2
+        self.choice1Destination = Choice1Result
+        self.choice2Destination = Choice2Result
+    }
+
+}
